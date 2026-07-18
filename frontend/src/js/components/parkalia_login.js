0a41535260b69e2ff02d8059f30d037989b06a56
Emanuel <XxEmmaTheCrackxX@gmail.com>
2026-07-17 22:40:15 -0500

    Feat Emanuel: Primer commit estructura frontend

---
+++ frontend/src/js/components/parkalia_login.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formLoginContainer = document.querySelector(".parkalia-login-container");
+
+    if (!formLoginContainer) return;
+
+    fetch("/frontend/src/html/components/parkalia_login.html")
+        .then(res => res.text())
+        .then(html => {
+            formContainer.innerHTML = html;
+            initLogin();
+        })
+        .catch(err => console.error(err));
+});
